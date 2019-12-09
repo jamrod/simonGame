1783ec631acbf8a9deae8b5659b53849368bea8b
Jamrod <myfavoritenoisemaker@gmail.com>
2019-12-09 09:13:14 -0700

    working on class instantiation

--- script.js
+++ script.js
@@ -16,24 +16,59 @@ const scoreDisplay = document.querySelector("#score-display")
 const currentStatus = document.querySelector("#current-status")
 const startButton = document.querySelector("#start-button")
 const highScoreDisplay = document.querySelector("#highscore-display")
-const redButton = document.querySelector("#red")
-const yellowButton = document.querySelector("#yellow")
-const blueButton = document.querySelector("#blue")
-const orangeButton = document.querySelector("#orange")
-const greenButton = document.querySelector("#green")
-const violetButton = document.querySelector("#violet")
+// const redButton = document.querySelector("#red")
+// const yellowButton = document.querySelector("#yellow")
+// const blueButton = document.querySelector("#blue")
+// const orangeButton = document.querySelector("#orange")
+// const greenButton = document.querySelector("#green")
+// const violetButton = document.querySelector("#violet")
 
+let buttonArray = []
 let sequence = []
 let currentCount = 0
 let watching = true
- 
-startButton.addEventListener('click', startGame(evt))
-gameContainer.addEventListener('click', handleClick(evt))
+
+startButton.addEventListener('click', evt => {startGame(evt)})
+// gameContainer.addEventListener('click', evt => {handleClick(evt)})
+
+class GameButton {
+    constructor(tag){
+        this.tag = tag
+        this.str = `"#{this.tag}"`
+        this.div = document.querySelector(this.str)
+    }
+    flashButton() {
+        this.div.backgroundColor = `"${this.tag}"`
+        setTimeout(this.clearButton, 1000)
+    }
+    clearButton() {
+        this.div.backgroundColor = "white"
+    }
+}
+
+ gameContainer.childNodes.forEach (button => {
+    tag = button.id
+    console.log(tag)
+    newButton = new GameButton(tag)
+    buttonArray.push(newButton)
+ })
 
 function startGame(evt) {
-    console.log(click)
+    console.log(evt)
+    flashSquare()
 }
 
 function handleClick(evt) {
     console.log(evt)
+
 }
+
+// function clearSquare() {
+//     redButton.style.backgroundColor = "white"
+// }
+
+// function flashSquare(){
+//     redButton.style.backgroundColor = "red"
+//     setTimeout(clearSquare, 1000)
+// }
+// redButton.style.backgroundColor = "red"
